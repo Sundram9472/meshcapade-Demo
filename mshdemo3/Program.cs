5ae1ddc2eef9d3d461106bd15e36909c9192141b
sundram <sundramtpvc@gmail.com>
2024-06-28 20:27:34 +0530

    initial commit

---
+++ mshdemo3/Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using mshdemo3;
+using mshdemo3.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.meshcapade.com/api/v1/") });
+builder.Services.AddBlazorBootstrap();
+builder.Services.AddScoped<AvatarService>();
+await builder.Build().RunAsync();
---
+++ mshdemo3/Services/AvatarService.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using mshdemo3.Modal;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static System.Net.WebRequestMethods;
+
+namespace mshdemo3.Services
+{
+    public class AvatarService
+    {
+        private readonly HttpClient _httpClient;
+        public AvatarService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> InitiateAvatarCreation(string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.PostAsync("avatars/create/from-images", null);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadFromJsonAsync<ApiResponse>();
+            return result.Data.Id.ToString();
+        }
+
+        public async Task<string> RequestImageUploads(string assetId, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.PostAsync($"avatars/{assetId}/images", null);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var uploadResponse = System.Text.Json.JsonSerializer.Deserialize<UploadResponse>(responseContent, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            var result = uploadResponse.data.attributes.url.path;
+            return result.ToString();
+        }
+
+        public async Task<bool> UploadImageToS3(string uploadUrl, Stream imageStream)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Put, uploadUrl);
+            using (var content = new StreamContent(imageStream))
+            {
+                request.Content = content;
+                var response = await client.SendAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public async Task StartFittingProcess(string assetId, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.PostAsync($"avatars/{assetId}/fit-to-images", null);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task<AvatarMeasurements> RetrieveAvatarMeasurements(string assetId, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.GetAsync($"avatars/{assetId}");
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadFromJsonAsync<AvatarMeasurements>();
+            return result;
+        }
+
+        public async Task<AvatarExport> ExportAvatar(string assetId, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var requestBody = new
+            {
+                format = "OBJ",
+                pose = "a"
+            };
+            var response = await _httpClient.PostAsJsonAsync($"avatars/{assetId}/export", requestBody);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadFromJsonAsync<AvatarExport>();
+            return result;
+        }
+
+    }
+
+    public class ApiResponse
+    {
+        public ApiData Data { get; set; }
+    }
+    public class ApiData
+    {
+        public string Id { get; set; }
+    }
+}
